2b8f457bf5f8ee55d04f3f249ee17f111bb04007
Eugen Donos <donos.eugen@jivygroup.com>
2018-11-09 15:38:39 +0200

    Added classes

---
+++ ConsoleApp2/Matrix.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace ConsoleApp2
+{
+    internal partial class Program
+    {
+        public class Matrix
+        {
+            private readonly int _matrixRows;
+            private readonly int _matrixColumns;
+            private readonly char[,] initialMatrix;
+
+            public Matrix(int rows, int columns)
+            {
+                _matrixRows = rows;
+                _matrixColumns = columns;
+
+                initialMatrix = new char[_matrixRows, _matrixColumns];
+            }
+
+
+            public void Initialize(char x, char y)
+            {
+                Random randomNum = new Random();
+
+                for (int i = 0; i < _matrixRows; i++)
+                {
+                    for (int j = 0; j < _matrixColumns; j++)
+                    {
+                        if (randomNum.NextDouble() >= 0.5)
+                        {
+                            initialMatrix[i, j] = x;
+                        }
+                        else
+                        {
+                            initialMatrix[i, j] = y;
+                        }
+                    }
+                }
+
+
+            }
+
+            public void Print()
+            {
+
+                Console.WriteLine("Print the matrix :");
+                for (int i = 0; i < _matrixRows; i++)
+                {
+                    for (int j = 0; j < _matrixColumns; j++)
+                    {
+                        Console.Write(" " + initialMatrix[i, j]);
+                    }
+                    Console.WriteLine("");
+                }
+
+                System.Threading.Thread.Sleep(500);
+                Console.Clear();
+            }
+
+            public List<List<char>> GetNeighboursNonPrimitive()
+            {
+
+                List<List<char>> neighbors = new List<List<char>>();
+
+
+                for (int i = 0; i < _matrixRows; i++)
+                {
+                    for (int j = 0; j < _matrixColumns; j++)
+                    {
+                        List<char> list = new List<char>();
+                        for (int a = i - 1; a <= i + 1; a++)
+                        {
+                            if (a < 0 || a >= _matrixRows)
+                            {
+                                continue;
+                            }
+
+                            for (int b = j - 1; b <= j + 1; b++)
+                            {
+                                if (b < 0 || b >= _matrixColumns)
+                                {
+                                    continue;
+                                }
+
+                                if (a != i || b != j)
+                                {
+                                    list.Add(initialMatrix[a, b]);
+                                }
+                            }
+
+                        }
+                        neighbors.Add(list);
+                    }
+
+                }
+
+                return neighbors;
+            }
+
+            public void RecalculateMatrix(List<List<char>> neighbors, char dead, char alive)
+            {
+                List<int> liveCells = CountLiveCells(neighbors, alive);
+                char[,] updatedMatrix = new char[_matrixRows, _matrixColumns];
+                int incr = 0;
+
+
+                for (int i = 0; i < _matrixRows; i++)
+                {
+                    for (int j = 0; j < _matrixColumns; j++)
+                    {
+                        //cell birth
+                        if (liveCells[incr] == 3)
+                        {
+                            initialMatrix[i, j] = alive;
+                        }
+
+                        //cell death
+                        if (liveCells[incr] < 2 || liveCells[incr] > 3)
+                        {
+                            initialMatrix[i, j] = dead;
+                        }
+
+                        incr++;
+                    }
+                }
+            }
+
+            public List<int> CountLiveCells(List<List<char>> neighbors, char alive)
+            {
+                List<int> liveCellsList = new List<int>();
+
+                //get for each array in list the alive cells
+                foreach (List<char> n in neighbors)
+                {
+
+                    liveCellsList.Add(n.FindAll(x => x == alive).Count);
+                }
+
+                return liveCellsList;
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
+//1.Make 1/0 in matrix to be x/y or any other symbol
+//2. Separate to class
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,141 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+
+
 
 namespace ConsoleApp2
 {
-    internal class Program
+    internal partial class Program
     {
         private static void Main(string[] args)
         {
+            char deadCellNotation = ' ';
+            char aliveCellNotation = '*';
 
-            int[,] initialMatrix = new int[10, 10];
+            Matrix matrix = new Matrix(10, 10);
 
-            //Initial board randomized
-            InitializeMatrix(initialMatrix);
+            ////Initial board randomized
+            matrix.Initialize(deadCellNotation, aliveCellNotation);
 
             while (true)
             {
-                PrintMatrix(initialMatrix);
+                matrix.Print();
 
-                List<List<int>> neighbors = GetNeighborsNonPrimitive(initialMatrix);
+                List<List<char>> neighbors = matrix.GetNeighboursNonPrimitive();
 
-                RecalculateMatrix(initialMatrix, neighbors);
-
-            }
-        }
-
-        public static void InitializeMatrix(int[,] matrix)
-        {
-            Random randomNum = new Random();
-
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    matrix[i, j] = randomNum.Next(2);
-                }
-            }
-        }
-
-        public static void PrintMatrix(int[,] matrix)
-        {
-            int rowLenght = matrix.GetLength(0);
-            int columnLenght = matrix.GetLength(1);
-
-            Console.WriteLine("Print the matrix :");
-            for (int i = 0; i < rowLenght; i++)
-            {
-                for (int j = 0; j < columnLenght; j++)
-                {
-                    Console.Write(" " + matrix[i, j]);
-                }
-                Console.WriteLine("");
-            }
-
-            System.Threading.Thread.Sleep(500);
-            Console.Clear();
-        }
-
-        public static List<List<int>> GetNeighborsNonPrimitive(int[,] matrix)
-        {
-
-            List<List<int>> neighbors = new List<List<int>>();
-
-
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    List<int> list = new List<int>();
-                    for (int a = i - 1; a <= i + 1; a++)
-                    {
-                        if (a < 0 || a >= matrix.GetLength(0))
-                        {
-                            continue;
-                        }
-
-                        for (int b = j - 1; b <= j + 1; b++)
-                        {
-                            if (b < 0 || b >= matrix.GetLength(1))
-                            {
-                                continue;
-                            }
-
-                            if (a != i || b != j)
-                            {
-                                list.Add(matrix[a, b]);
-                            }
-                        }
-
-                    }
-                    neighbors.Add(list);
-                }
-
-            }
-
-            return neighbors;
-        }
-
-        public static List<int> CountLiveCells(List<List<int>> neighbors)
-        {
-            List<int> liveCellsList = new List<int>();
-
-            //get for each array in list the numbers of 1 values
-            foreach (List<int> n in neighbors)
-            {
-                liveCellsList.Add(n.FindAll(x => x == 1).Count);
-            }
-
-            return liveCellsList;
-        }
-
-        public static void RecalculateMatrix(int[,] matrix, List<List<int>> neighbors)
-        {
-            List<int> liveCells = CountLiveCells(neighbors);
-            int[,] updatedMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
-            int incr = 0;
-
-
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    //cell birth
-                    if (liveCells[incr] == 3)
-                    {
-                        matrix[i, j] = 1;
-                    }
-
-                    //cell death
-                    if (liveCells[incr] < 2 || liveCells[incr] > 3)
-                    {
-                        matrix[i, j] = 0;
-                    }
-
-                    incr++;
-                }
+                matrix.RecalculateMatrix(neighbors, deadCellNotation, aliveCellNotation);
             }
         }
     }
 }
-
