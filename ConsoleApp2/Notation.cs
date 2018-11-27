4ee8cf2f949b4aff9534d4e00c61282081464628
Eugen Donos <donos.eugen@jivygroup.com>
2018-11-27 12:46:41 +0200

    Changes to CellNotation

--- ConsoleApp2/Notation.cs
+++ ConsoleApp2/Notation.cs
@@ -5,14 +5,38 @@
         private char _aliveNotation;
         private char _deadNotation;
 
-        public char AliveNotation { get => _aliveNotation; set => _aliveNotation = value; }
-        public char DeadNotation { get => _deadNotation; set => _deadNotation = value; }
+        public char AliveCellNotation { get => _aliveNotation; set => _aliveNotation = value; }
+        public char DeadCellNotation { get => _deadNotation; set => _deadNotation = value; }
 
 
         public Notations()
         {
-            AliveNotation = System.Configuration.ConfigurationManager.AppSettings["AliveCell"].ToCharArray()[0];
-            DeadNotation = System.Configuration.ConfigurationSettings.AppSettings["DeadCell"].ToCharArray()[0];
+            AliveCellNotation = GetNotationForAliveCell();
+            DeadCellNotation = GetNotationForDeadCell();
         }
+
+
+        private char GetNotationForAliveCell()
+        {
+            var appConfigPath = System.Configuration.ConfigurationManager.AppSettings["AliveCell"];
+            if (string.IsNullOrEmpty(appConfigPath))
+            {
+                return ' ';
+            }
+            else return appConfigPath.ToCharArray()[0];
+        }
+
+        private char GetNotationForDeadCell()
+        {
+            var appConfigPath = System.Configuration.ConfigurationManager.AppSettings["DeadCell"];
+            if (string.IsNullOrEmpty(appConfigPath))
+            {
+                return ' ';
+            }
+            else return appConfigPath.ToCharArray()[0];
+        }
+
+
+
     }
 }
